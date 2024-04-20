b6a42f7b9b8cbede812f5448656b2f0210805b73
sangyoon kim <sangyun0904@gmail.com>
2024-04-20 19:42:32 +0900

    CS50 Makefile 추가, hello.c 수정

--- lesson2/hello.c
+++ lesson2/hello.c
@@ -1,6 +1,9 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    printf("hello, world\n");
+    string first = get_string("What's your first name? ");
+    string last = get_string("What's your last name? ");
+    printf("hello, %s %s!\n", first, last);
 }
