3db97811d29887fede5367390161f6a534d0a3ff
sangyoon kim <sangyun0904@gmail.com>
2024-04-21 18:49:06 +0900

    comapre.c, agree.c meow.c 추가

---
+++ lesson2/meow.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    int i = 0;
+    while (i < 3) 
+    {
+        printf("meow\n");
+        i++;
+    }
+
+    for (int i = 0; i < 3; i++) 
+    {
+        printf("meow\n");
+    }
+
+    while (true) 
+    {
+        printf("meow\n");
+    }
+}
