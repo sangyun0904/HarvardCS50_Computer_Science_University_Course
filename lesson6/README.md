6c34bf7d4e700c63fdb40eed73ffe8dbc97ebe2f
sangyoon kim <sangyun0904@gmail.com>
2024-05-05 21:11:21 +0900

    lesson6 README.md, list.c 링크드 리스트 내용 추가

--- lesson6/README.md
+++ lesson6/README.md
@@ -13,3 +13,15 @@ data out : pop
 ### Linked List 
 
 C에서 링크드리스트를 구현하기 위해서는 값과 함께 다음 노트를 가리키는 포인터를 저장한다.  (Single Linked List)
+
+```
+typedef struct
+{
+    int number;
+    // node 안에 다시 node를 사용하기 위해선 struct을 추가해주어야 한다. 
+    struct node *next; // address of the next node
+}
+node;
+```
+
+링크드 리스트를 사용하면 데이터를 비어있는 곳 아무곳이나 저장할 수 있고, 즉 C에서 Array에 데이터를 추가로 저장할 때 처럼 realloc 해줄 필요도 없다 그러나 매번 데이터를 저장할 때마다 다음 노드의 주소를 같이 저장해주어야 하기 때문에 메모리가 더 많이 필요하다. 또한 링크드 리스트는 인덱스를 설정할 수도 없다. 이로 인해 이진 탐색과 같은 알고리즘은 링크드 리스트 자료구조에 사용할 수 없다. 
--- lesson6/list.c
+++ lesson6/list.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) 
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
+int main(int argc, char *argv[]) 
 {
 
     int *list = malloc(3 * sizeof(int));
@@ -36,5 +43,49 @@ int main(void)
     }
 
     free(list);
-    return 0;
+
+    // Linked List
+    node *linked_list = NULL; 
+
+    for (int i = 1; i < argc; i++)
+    {
+        int number = atoi(argv[i]);
+
+        node *n = malloc(sizeof(node));
+        // 이 부분에서 만역 한두번은 malloc으로 메모리를 할당받는것을 성공한 후에 NULL 값을 받고 리턴하면 
+        // 사용하지 않는 할당된 메모리 생기게 된다.  
+        // 그래서 free list 같은 함숙를 따로 만들어서 모든 메모리를 free 해주는것이 옳은 방법이다. 
+        if (n == NULL)
+        {
+            return 1;
+        }
+        // n->number = (*n).number
+        n->number = number;
+        n->next = NULL;
+
+        n->next = linked_list;
+        linked_list = n;
+    }
+    
+    node *ptr = linked_list;
+    while (ptr != NULL)
+    {
+        printf("ptr : %i\n", ptr->number);
+        ptr = ptr->next;
+    }
+
+    printf("\n");
+
+    for (node *ptr = linked_list; ptr != NULL; ptr = ptr->next)
+    {
+        printf("ptr : %i\n", ptr->number);
+    }
+
+    ptr = linked_list;
+    while (ptr != NULL)
+    {
+        node *next = ptr->next;
+        free(ptr);
+        ptr = next;
+    }
 }
